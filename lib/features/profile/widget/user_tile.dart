7426e8d8b41703c7437f47c717afc0b49cf0fe76
Mehmet Fidan <85038013+mettermizm@users.noreply.github.com>
2024-10-06 23:35:59 +0300

    Bottom Sheet ekledim fakat çalıştıramadım

--- lib/features/profile/widget/user_tile.dart
+++ lib/features/profile/widget/user_tile.dart
@@ -40,7 +40,36 @@ final class UserTile extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(width: 5),
-                  IconConstants.verify.toCustomIcon(size: 20),
+                  GestureDetector(
+                      onTap: () {
+                        showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
+                          barrierColor: Colors.white38,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: const BorderRadius.vertical(
+                                    top: Radius.circular(50)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.15),
+                                    spreadRadius: 5,
+                                    blurRadius: 15,
+                                    offset: const Offset(0, -5),
+                                  ),
+                                ],
+                              ),
+                              child: BottomSheetCustom.buildBottomSheetMenu(
+                                context,
+                                'creator.username',
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      child: IconConstants.verify.toCustomIcon(size: 20)),
                   const SizedBox(width: 5),
                   Text(
                     '@${user.userName}',
@@ -70,6 +99,119 @@ final class UserTile extends StatelessWidget {
           child: IconConstants.more.toIcon,
         ),
       ],
+    );
+  }
+}
+
+class BottomSheetCustom {
+  static Widget buildBottomSheetMenu(BuildContext context, String username) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            leading: Image.asset("GetImage.bottomSheetPaylas",
+                width: 24, height: 24),
+            title: Text(
+              'Anketi paylaş',
+              style: TextStyle(
+                fontFamily: FontConstants.gilroyMedium,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+            child: Divider(height: 1),
+          ),
+          ListTile(
+            leading: Image.asset("GetImage.bottomSheetTakipBirak",
+                width: 24, height: 24),
+            title: Text(
+              '@$username adlı kişiyi takibi bırak',
+              style: TextStyle(
+                fontFamily: FontConstants.gilroyMedium,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading:
+                Image.asset("GetImage.bottomSheetEngel", width: 24, height: 24),
+            title: Text(
+              '@$username adlı kişiyi engelle',
+              style: TextStyle(
+                fontFamily: FontConstants.gilroyMedium,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Image.asset("GetImage.bottomSheetSikayet",
+                width: 24, height: 24),
+            title: Text(
+              '@$username adlı kişiyi şikayet et',
+              style: TextStyle(
+                fontFamily: FontConstants.gilroyMedium,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Image.asset("GetImage.bottomSheetSessizeAl",
+                width: 24, height: 24),
+            title: Text(
+              '@$username adlı kişiyi sessize al',
+              style: TextStyle(
+                fontFamily: FontConstants.gilroyMedium,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+            child: Divider(height: 1), // Divider'ı ayarladık
+          ),
+          ListTile(
+            leading: Image.asset("GetImage.bottomSheetBildir",
+                width: 24, height: 24),
+            title: Text(
+              'Anketi bildir',
+              style: TextStyle(
+                fontFamily: FontConstants.gilroyMedium,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+              // Anket bildirme fonksiyonu buraya eklenecek
+            },
+          ),
+        ],
+      ),
     );
   }
 }
